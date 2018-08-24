2e28084ad0eb4bee842b155ff440a7eaed6cdcb8
Alexander Q <qalexx@gmail.com>
2018-08-24 18:54:23 +0300

    working stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,62 @@
 extern crate libc;
-use libc::c_double;
+
+use libc::{c_double, c_void};
+use std::ffi::CString;
+use std::ffi::CStr;
 use std::os::raw::c_char;
 
-pub enum Tdlib {}
+type TdlibClient = *mut c_void;
+
+#[derive(Debug)]
+pub struct Tdlib {
+    instance: TdlibClient
+}
 
 #[link(name = "tdjson")]
 extern {
-    fn td_json_client_create() -> *mut Tdlib;
-    fn td_json_client_send(client: *mut Tdlib, request: *const c_char);
-    fn td_json_client_receive(client: *mut Tdlib, timeout: c_double) -> *mut c_char;
-    fn td_json_client_execute(client: *mut Tdlib, request: *const c_char) -> *mut c_char;
-    fn td_json_client_destroy(client: *mut Tdlib);
+    fn td_json_client_create() -> TdlibClient;
+    fn td_json_client_send(client: TdlibClient, request: *const c_char);
+    fn td_json_client_receive(client: TdlibClient, timeout: c_double) -> *mut c_char;
+    fn td_json_client_execute(client: TdlibClient, request: *const c_char) -> *mut c_char;
+    fn td_json_client_destroy(client: TdlibClient);
 }
 
 impl Tdlib {
-    pub fn create() -> *mut Self {
-        unsafe { td_json_client_create() }
+    pub fn new() -> Self {
+        let client = unsafe { td_json_client_create() };
+        Tdlib { instance: client }
+    }
+
+    pub fn send(&self, request: &str) {
+        println!("sending: {}", request);
+        let cstring = CString::new(request).unwrap();
+        unsafe { td_json_client_send(self.instance, cstring.as_ptr()) }
+    }
+
+    pub fn execute(&self, request: &str) -> String {
+        println!("executing: {}", request);
+        let cstring = CString::new(request).unwrap();
+        unsafe {
+            let response = td_json_client_execute(self.instance, cstring.as_ptr());
+            CStr::from_ptr(response).to_string_lossy().into_owned()
+        }
+    }
+
+    pub fn receive(&self, timeout: f64) -> Option<String> {
+        println!("receiving with timeout {}", timeout);
+        unsafe {
+            td_json_client_receive(self.instance, timeout)
+                .as_ref()
+                .map(|response| CStr::from_ptr(response).to_string_lossy().into_owned())
+        }
     }
 }
 
 impl Drop for Tdlib {
     fn drop(&mut self) {
         unsafe {
-            println!("dropping client");
-            td_json_client_destroy(self);
+            println!("dropping client"); // XXX debug
+            td_json_client_destroy(self.instance);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,54 @@
 extern crate rust_tdlib;
+extern crate json;
 
+use json::object;
 use rust_tdlib::Tdlib;
+use std::env;
+use std::io;
 
 fn main() {
     println!("Hello, world!");
 
-    {
-        let client = Tdlib::create();
-        // let client = rust_tdlib::client_create();
-        println!("client: {:?}", client);
-        //TODO doesn't drop here for some reason
+    let args: Vec<String> = env::args().collect();
+
+    let app_id = &args[1];
+    let app_hash = &args[2];
+
+    let client = Tdlib::new();
+    println!("client: {:?}", client);
+
+    let cfg = config(app_id, app_hash);
+    let req = json::stringify( cfg );
+
+    client.send(&req[..]);
+
+    loop {
+        let resp = client.receive(2.0);
+        println!("resp: {:?}", resp);
     }
-    // unsafe { td_json_client_destroy(client); }
-    // println!("client: {:?}", client);
-    println!("ok")
+}
+
+fn config(api_id: &str, api_hash: &str) -> json::JsonValue {
+    let cfg = object!{
+        "@type" => "tdlibParameters",
+        "api_hash" => api_hash,
+        "api_id" => api_id,
+        "application_version" => "Unknown",
+        "database_directory" => "tdlib",
+        "device_model" => "Unknown",
+        "enable_storage_optimizer" => true,
+        "files_directory" => json::Null,
+        "ignore_file_names" => true,
+        "system_language_code" => "en",
+        "system_version" => "Unknown",
+        "use_chat_info_database" => true,
+        "use_file_database" => true,
+        "use_secret_chats" => false,
+        "use_test_dc" => true
+    };
+
+    object!{
+        "@type" => "setTdlibParameters",
+        "parameters" => cfg
+    }
 }
